52abd4ffe10492405c2978bb3ee38d8397a3e922
Filipe Coutinho <fpcoutinho5@gmail.com>
2022-09-28 12:04:21 -0300

    formulario e modelo 100% completos

--- relatorio/forms.py
+++ relatorio/forms.py
@@ -1,18 +1,44 @@
 from email.policy import default
 from django import forms
 from . import models
-from django.forms.widgets import DateTimeInput, RadioSelect
+from django.forms.widgets import DateTimeInput, RadioSelect, CheckboxSelectMultiple
 
 
 class CriaRelatorioForm(forms.ModelForm):
     data = forms.DateTimeField(widget=DateTimeInput(attrs={'type': 'datetime-local'}),input_formats='%d/%m/%Y %H:%M')
+    #conjunto de forms de sim/nao
     radio=[('Sim','Sim'), ('Não','Não')]
-    quali = [('EE','Engenheiro Eletricista')]
     especialista = forms.ChoiceField(choices=radio, widget=forms.RadioSelect)
+    desligamento = forms.ChoiceField(choices=radio, widget=forms.RadioSelect)
+    integridade = forms.ChoiceField(choices=radio, widget=forms.RadioSelect)
+    dialogo = forms.ChoiceField(choices=radio, widget=forms.RadioSelect)
+    curso_nr = forms.ChoiceField(choices=radio, widget=forms.RadioSelect)
+    conferido = forms.ChoiceField(choices=radio, widget=forms.RadioSelect)
+    delimitar_area = forms.ChoiceField(choices=radio, widget=forms.RadioSelect)
+    auxconces = forms.ChoiceField(choices=radio, widget=forms.RadioSelect)
+    tensao = forms.ChoiceField(choices=radio, widget=forms.RadioSelect)
+    aterramento = forms.ChoiceField(choices=radio, widget=forms.RadioSelect)
+    altura = forms.ChoiceField(choices=radio, widget=forms.RadioSelect)
+    cinto_seg = forms.ChoiceField(choices=radio, widget=forms.RadioSelect)
+    requi_seg = forms.ChoiceField(choices=radio, widget=forms.RadioSelect)
+    reavaliacao = forms.ChoiceField(choices=radio, widget=forms.RadioSelect)
     
+    quali = [('EE','Engenheiro Eletricista'), ('TE', 'Técnico Eletrotécnico'), ('EL','Eletricista'), ('AL', 'Aluno de curso profissionalizante')]
+    Qualificacao_Profissional = forms.ChoiceField(choices=quali, widget=forms.RadioSelect)
+   
+    #forms de multipla escolha
+    risk = [('QD','Queda'), ('EX', 'Explosão'), ('ER','Ergonômico'), ('AP', 'Animais Peçonhentos'), ('AV','Arco Voltaico'), ('AT','Atropelamento'), ('RU','Ruído'), ('CH','Choque')]
+    riscos = forms.MultipleChoiceField(choices=risk, widget=forms.CheckboxSelectMultiple())
+
+    eqp = [('CP','Capacete'), ('MI','Manga Isolante'), ('BD', 'Botina Dielétrica'), ('LC','Luva de Cobertura'), ('OP','Óculos de Proteção'), ('PA','Protetor Auricular'), ('LB','Luva de Borracha Isolante'), ('CS','Cinto de Segurança')]
+    equipamentos = forms.MultipleChoiceField(choices=eqp, widget=forms.CheckboxSelectMultiple())
+
+    silz = [('CN','Cone'),('GF','Giroflex'),('FI','Fita para Isolamento da área'), ('SS','Sinaleira Sonora'), ('CV','Cavaletes'), ('NN','Nenhum')]
+    sinalizacao = forms.MultipleChoiceField(choices=silz, widget=forms.CheckboxSelectMultiple())
+
     class Meta:
         model = models.Relatorio
-        fields = ['nome', 'local', 'data', 'temperatura', 'clima', 'responsaveis', 'especialista']
+        fields = ['nome', 'local', 'data', 'temperatura', 'clima', 'responsaveis','Qualificacao_Profissional','riscos', 'equipamentos', 'sinalizacao', 'desligamento', 'especialista', 'integridade', 'dialogo', 'curso_nr', 'conferido', 'delimitar_area', 'auxconces', 'tensao', 'aterramento', 'altura', 'cinto_seg', 'requi_seg', 'reavaliacao']
         
 
 
@@ -20,7 +46,33 @@ class EditaRelatorioForm(forms.ModelForm):
     data = forms.DateTimeField(widget=DateTimeInput(format='%d/%m/%Y %H:%M', attrs={'type': 'datetime-local'}), input_formats='%d/%m/%Y %H:%M')
     radio=[('Sim','Sim'), ('Não','Não')]
     especialista = forms.ChoiceField(choices=radio, widget=forms.RadioSelect)
+    desligamento = forms.ChoiceField(choices=radio, widget=forms.RadioSelect)
+    integridade = forms.ChoiceField(choices=radio, widget=forms.RadioSelect)
+    dialogo = forms.ChoiceField(choices=radio, widget=forms.RadioSelect)
+    curso_nr = forms.ChoiceField(choices=radio, widget=forms.RadioSelect)
+    conferido = forms.ChoiceField(choices=radio, widget=forms.RadioSelect)
+    delimitar_area = forms.ChoiceField(choices=radio, widget=forms.RadioSelect)
+    auxconces = forms.ChoiceField(choices=radio, widget=forms.RadioSelect)
+    tensao = forms.ChoiceField(choices=radio, widget=forms.RadioSelect)
+    aterramento = forms.ChoiceField(choices=radio, widget=forms.RadioSelect)
+    altura = forms.ChoiceField(choices=radio, widget=forms.RadioSelect)
+    cinto_seg = forms.ChoiceField(choices=radio, widget=forms.RadioSelect)
+    requi_seg = forms.ChoiceField(choices=radio, widget=forms.RadioSelect)
+    reavaliacao = forms.ChoiceField(choices=radio, widget=forms.RadioSelect)
+    
+    quali = [('EE','Engenheiro Eletricista'), ('TE', 'Técnico Eletrotécnico'), ('EL','Eletricista'), ('AL', 'Aluno de curso profissionalizante')]
+    Qualificacao_Profissional = forms.ChoiceField(choices=quali, widget=forms.RadioSelect)
+
+    #forms de multipla escolha
+    risk = [('QD','Queda'), ('EX', 'Explosão'), ('ER','Ergonômico'), ('AP', 'Animais Peçonhentos'), ('AV','Arco Voltaico'), ('AT','Atropelamento'), ('RU','Ruído'), ('CH','Choque')]
+    riscos = forms.MultipleChoiceField(choices=risk, widget=forms.CheckboxSelectMultiple())
+
+    eqp = [('CP','Capacete'), ('MI','Manga Isolante'), ('BD', 'Botina Dielétrica'), ('LC','Luva de Cobertura'), ('OP','Óculos de Proteção'), ('PA','Protetor Auricular'), ('LB','Luva de Borracha Isolante'), ('CS','Cinto de Segurança')]
+    equipamentos = forms.MultipleChoiceField(choices=eqp, widget=forms.CheckboxSelectMultiple())
+
+    silz = [('CN','Cone'),('GF','Giroflex'),('FI','Fita para Isolamento da área'), ('SS','Sinaleira Sonora'), ('CV','Cavaletes'), ('NN','Nenhum')]
+    sinalizacao = forms.MultipleChoiceField(choices=silz, widget=forms.CheckboxSelectMultiple())
 
     class Meta:
         model = models.Relatorio
-        fields = ['nome', 'local', 'data', 'temperatura', 'clima', 'responsaveis', 'especialista']
+        fields = ['nome', 'local', 'data', 'temperatura', 'clima', 'responsaveis','Qualificacao_Profissional','riscos', 'equipamentos', 'sinalizacao', 'desligamento', 'especialista', 'integridade', 'dialogo', 'curso_nr', 'conferido', 'delimitar_area', 'auxconces', 'tensao', 'aterramento', 'altura', 'cinto_seg', 'requi_seg', 'reavaliacao']
--- relatorio/models.py
+++ relatorio/models.py
@@ -5,14 +5,31 @@ from django.contrib.auth.models import User
 
 class Relatorio(models.Model):
 
+    autor = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
     nome = models.CharField(max_length=100)
     local = models.CharField(max_length=100)
     data = models.DateTimeField()
     temperatura = models.IntegerField()
     clima = models.CharField(max_length=100)
     responsaveis = models.TextField(blank=True)
+    Qualificacao_Profissional = models.CharField(max_length=100, default="Sim")
+    riscos = models.CharField(max_length=100, default="Sim")
+    equipamentos = models.CharField(max_length=100, default="Sim")
+    sinalizacao = models.CharField(max_length=100, default="Sim")
+    desligamento = models.CharField(max_length=4, default="Sim")
     especialista = models.CharField(max_length=4, default="Sim")
-    autor = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
+    integridade = models.CharField(max_length=4, default="Sim")
+    dialogo = models.CharField(max_length=4, default="Sim")
+    curso_nr = models.CharField(max_length=4, default="Sim")
+    conferido = models.CharField(max_length=4, default="Sim")
+    delimitar_area = models.CharField(max_length=4, default="Sim")
+    auxconces = models.CharField(max_length=4, default="Sim")
+    tensao = models.CharField(max_length=4, default="Sim")
+    aterramento = models.CharField(max_length=4, default="Sim")
+    altura = models.CharField(max_length=4, default="Sim")
+    cinto_seg = models.CharField(max_length=4, default="Sim")
+    requi_seg = models.CharField(max_length=4, default="Sim")
+    reavaliacao = models.CharField(max_length=4, default="Sim")
 
     def __str__(self):
         return self.nome
