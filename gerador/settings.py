a1456212ee8ade064b8bbf7f65c345c4b8589f2f
Filipe Coutinho <fpcoutinho5@gmail.com>
2022-09-28 13:25:45 -0300

    img

--- gerador/settings.py
+++ gerador/settings.py
@@ -131,6 +131,9 @@ STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'assets'),
 ]
 
+MEDIA_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
