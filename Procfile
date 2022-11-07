39bf8babdbd66f5ce308c8b7089f407f90e96771
Filipe Coutinho <fpcoutinho5@gmail.com>
2022-11-07 16:41:24 -0300

    Configuração Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn gerador.wsgi
--- gerador/settings.py
+++ gerador/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os.path
 from pathlib import Path
 from django.contrib.messages import constants as messages
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -140,3 +141,5 @@ MEDIA_URL = 'media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
